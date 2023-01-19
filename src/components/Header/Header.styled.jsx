889a04bae746dae28f3ab7cf58449f35f9a3b695
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-01-19 13:10:50 +0200

    Update Header.styled.jsx

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -8,7 +8,7 @@ export const HeaderStyled = styled.header`
   height: 60px;
   margin-bottom: 20px;
   padding: 20px 40px;
-  background: grey;
+  background: black;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
@@ -17,7 +17,7 @@ export const NavLinkStyled = styled(NavLink)`
   font-size: 18px;
   font-weight: bold;
   text-decoration: none;
-  color: black;
+  color: white;
   &:not(:last-child) {
     margin-right: 20px;
   }
